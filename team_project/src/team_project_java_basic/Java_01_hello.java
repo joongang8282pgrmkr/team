e6a4bc9e4dfc5da4082c19dd0c06daa82af96425
babo <babo@blabla.com>
2020-04-10 17:31:11 +0900

    1

--- team_project/src/team_project_java_basic/Java_01_hello.java
+++ team_project/src/team_project_java_basic/Java_01_hello.java
@@ -3,5 +3,6 @@ package team_project_java_basic;
 public class Java_01_hello {
 	public static void main(String[] args) {
 		System.out.println("hello java");
+		System.out.println("hello java");
 	}  
 }
