71202eeeab49990a11f5ab8dc060e3a141b5832b
babo <babo@blabla.com>
2020-04-10 17:18:17 +0900

    foreach 테스트

---
+++ team_project/src/team_project_another_emp/Test_foreach.java
@@ -0,0 +1,11 @@
+package team_project_another_emp;
+
+public class Test_foreach {
+	public static void main(String[] args) {
+		System.out.println("test foreach");
+		String[] str = {"1","2","3","4"};
+		for(String s : str) {
+			System.out.println(s);
+		}
+	}
+}
