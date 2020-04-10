fa5db0a82f8db602dfd4c1356df40916cb64c995
master <tt0401@navver.com>
2020-04-10 16:01:29 +0900

    new asc

--- team_project/src/team_project/Teamclass.java
+++ team_project/src/team_project/Teamclass.java
@@ -3,7 +3,12 @@ package team_project;
 public class Teamclass {
 	public static void main(String[] args) {
 		System.out.println("team");
-		System.out.println("team member1");
-		System.out.println("Hello world!");
+		System.out.println("Hello world1!");
+		System.out.println("Hello world2!");
+		System.out.println("Hello world3!");
+		System.out.println("Hello world4!");
+		System.out.println("Hello world5!");
+		System.out.println("Hello world6!");
+		System.out.println("Hello world7!");
 	}
 }
