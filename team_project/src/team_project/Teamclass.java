6431133a160b12f785ad38ce8135883a91510186
master <tt0401@navver.com>
2020-04-10 16:03:42 +0900

    for문

--- team_project/src/team_project/Teamclass.java
+++ team_project/src/team_project/Teamclass.java
@@ -3,12 +3,8 @@ package team_project;
 public class Teamclass {
 	public static void main(String[] args) {
 		System.out.println("team");
-		System.out.println("Hello world1!");
-		System.out.println("Hello world2!");
-		System.out.println("Hello world3!");
-		System.out.println("Hello world4!");
-		System.out.println("Hello world5!");
-		System.out.println("Hello world6!");
-		System.out.println("Hello world7!");
+
+		for (int i = 0; i < 7; i++)
+			System.out.println("Hello world" + (i + 1) + "!");
 	}
 }
