ee571312dc4a0403f441e6576f6bc5dbefc81ec8
master <tt0401@navver.com>
2020-04-10 15:36:00 +0900

    aa

--- team_project/src/team_project/Teamclass.java
+++ team_project/src/team_project/Teamclass.java
@@ -4,5 +4,6 @@ public class Teamclass {
 	public static void main(String[] args) {
 		System.out.println("team");
 		System.out.println("team member1");
+		System.out.println("team member2");
 	}
 }
